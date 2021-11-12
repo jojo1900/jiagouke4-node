f0b8b1510c73976dcf5cc7b4f68aa29c749902c7
Your Name <you@example.com>
2021-11-12 22:14:16 +0800

    node-core

--- 2.promise/5.async-await.js
+++ 2.promise/5.async-await.js
@@ -1,9 +1,13 @@
 const fs = require("fs").promises;
 async function read() { // 生成器  固定写法 加个*
   // 很像同步的
-    let out1 = await fs.readFile("a1.txt", "utf8"); // 串行 底层就是递归 ， try{}catch在同步代码中捕获异常
-    let out2 = await fs.readFile(out1, "utf-8");
-    return out2;
+  try{
+let out1 = await fs.readFile("a1.txt", "utf8"); // 串行 底层就是递归 ， try{}catch在同步代码中捕获异常
+let out2 = await fs.readFile(out1, "utf-8");
+return out2;
+}catch(e){
+}
+
 }
 
 
--- 2.promise/5.source.js
+++ 2.promise/5.source.js
@@ -1,46 +1,50 @@
 const fs = require("fs").promises;
 
 const regeneratorRuntime = {
-    mark(outerFn){
-        return outerFn
-    },
-    wrap(innerFunc,outerFn){
-        // 核心就是 生成器会返回一个迭代器
-        const _context = {
-            next:0, // 状态机 不停的改变指针 最终将代码执行完毕
-            done:false,
-            abrupt(type,value){
-                _context.next = 'end'
-                 innerFunc(_context)
-                 this.value = value
-                 return value;
-            },
-            stop(){
-                this.done = true;
-                return this.value
-            }
-        }
+  mark(outerFn) {
+    return outerFn;
+  },
+  wrap(innerFunc, outerFn) {
+    // 核心就是 生成器会返回一个迭代器
+    const _context = {
+      next: 0, // 状态机 不停的改变指针 最终将代码执行完毕
+      done: false,
+      abrupt(type, value) {
+        _context.next = "end";
+        innerFunc(_context);
+        this.value = value;
+        return value;
+      },
+      stop() {
+        this.done = true;
+        return this.value;
+      },
+    };
+    return {
+      next(data) {
+        // 每次调用next 都会执行这个read$函数 inerFunc
+        _context.sent = data; // 每次用户调用next 会将数据传递过来
+        let value = innerFunc(_context);
+
         return {
-            next(data){ // 每次调用next 都会执行这个read$函数 inerFunc
-                _context.sent = data; // 每次用户调用next 会将数据传递过来
-                let value = innerFunc(_context);
-               
-                return {
-                    value,
-                    done:_context.done
-                }
-            },
-        }
-    }
-}
-var _marked = /*#__PURE__*/regeneratorRuntime.mark(read);
+          value,
+          done: _context.done,
+        };
+      },
+    };
+  },
+};
+var _marked = /*#__PURE__*/ regeneratorRuntime.mark(read);
 // polyfill
-function read() { // generator 最终被编译出来了 这个方法
+function read() {
+  // generator 最终被编译出来了 这个方法
   var out1, out2;
   return regeneratorRuntime.wrap(function read$(_context) {
-    while (1) { // 在编写代码的时候 如果一个函数 会执行多次 我们通常会用一个while(1) 来表识代码会走多次
-      switch (_context.prev = _context.next) { // next -》 prev 判断的是prev
-
+    while (1) {
+      // 在编写代码的时候 如果一个函数 会执行多次 我们通常会用一个while(1) 来表识代码会走多次
+      switch (
+        (_context.prev = _context.next) // next -》 prev 判断的是prev
+      ) {
         case 0:
           _context.next = 2;
           return fs.readFile("a.txt", "utf8");
@@ -63,23 +67,25 @@ function read() { // generator 最终被编译出来了 这个方法
   }, _marked);
 }
 // 流程控制 1 2 3 4  -> 减缓了then链
-function co(it){ // co的核心源码
-    return new Promise((resolve,reject)=>{
-        function next(data){
-            let {value,done} = it.next(data); // 拿到 done 是否完成 value就是本次yield的返回值
-            if(done){ // 如果解析完毕直接将结果作为co的成功结果
-                return resolve(value)
-            }
-            Promise.resolve(value).then(data=>{
-                next(data)
-            },reject)
-        }
-        next();
-    })
+function co(it) {
+  // co的核心源码
+  return new Promise((resolve, reject) => {
+    function next(data) {
+      let { value, done } = it.next(data); // 拿到 done 是否完成 value就是本次yield的返回值
+      if (done) {
+        // 如果解析完毕直接将结果作为co的成功结果
+        return resolve(value);
+      }
+      Promise.resolve(value).then((data) => {
+        next(data);
+      }, reject);
+    }
+    next();
+  });
 }
-co(read()).then(data=>{
-    console.log(data)
-})
+co(read()).then((data) => {
+  console.log(data);
+});
 
 // let it = read();
 // let { value, done } = it.next();
